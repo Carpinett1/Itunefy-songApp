155f86f51c01761c9ff7d068fea024d42b6af59e
Thiago Carpinetti <thiac12@gmail.com>
2023-06-23 12:41:16 -0300

    req4

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <>
-      <p>Trybetunes</p>
-      <Routes>
-        <Route path="/" element={ <Login /> } />
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+      <Route path="/" element={ <Layout /> }>
         <Route path="/search" element={ <Search /> } />
         <Route path="/album/:id" element={ <Album /> } />
         <Route path="/favorites" element={ <h1>Favorites</h1> } />
         <Route path="/profile" element={ <h1>Profile</h1> } />
         <Route path="/profile/edit" element={ <h1>ProfileEdit</h1> } />
         <Route path="/*" element={ <h1>Not Found</h1> } />
-      </Routes>
-    </>
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import { UserType } from '../types';
+import LoadingPage from './LoadingPage';
+
+function Header() {
+  const [user, setUser] = useState<UserType>({
+    name: '',
+    email: '',
+    image: '',
+    description: '',
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await getUser();
+      setUser(response);
+      setIsLoading(false);
+    };
+    fetchUser();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <LoadingPage />
+    );
+  }
+
+  return (
+    <header data-testid="header-component">
+      <h2>TrybeTunes</h2>
+      <nav>
+        <NavLink data-testid="link-to-search" to="/search">Pesquisar</NavLink>
+        <NavLink data-testid="link-to-favorites" to="/favorites">Favoritos</NavLink>
+        <NavLink data-testid="link-to-profile" to="/profile">Perfil</NavLink>
+      </nav>
+      <p data-testid="header-user-name">{user.name}</p>
+    </header>
+  );
+}
+
+export default Header;
